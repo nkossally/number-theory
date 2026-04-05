c94686d2a4357b83747f8f29548b185e26ecb9fd
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2026-04-05 12:53:59 -0400

    add algos

---
+++ congruences.py
@@ -0,0 +1,19 @@
+from gcd_euclidean_algorithm import get_gcd_euclidean_algorithm
+
+# get solutions for  ax = b mod(m)
+def get_congruences(a, b, m):
+    divisor = get_gcd_euclidean_algorithm(a, m)
+    if b % divisor != 0:
+        return []
+    curr = 0
+    while curr < m and a *curr %m != b:
+        curr += 1
+    res = []
+    for i in range(divisor):
+        res.append(curr + i * m//divisor)
+    return res
+
+print(get_congruences(2, 3, 5))  
+print(get_congruences(2, 3, 31))  
+print(get_congruences(4, 12, 32))  
+print(get_congruences(3, 7, 15))  
--- gcd_euclidean_algorithm.py
+++ gcd_euclidean_algorithm.py
@@ -1,8 +1,6 @@
-def euclidean_algorithm(a, b):
+def get_gcd_euclidean_algorithm(a, b):
     """Calculate the greatest common divisor of a and b using the Euclidean algorithm."""
     while b:
         a, b = b, a % b
     return a
 
-print(euclidean_algorithm(48, 18))  # Output: 6
-print(euclidean_algorithm(18, 48))  # Output: 6
